635e69d8ad3c63461ad6f24c76e87a58bb6e1c15
piotrsiwek <59854137+piotrsiwek@users.noreply.github.com>
2024-03-05 02:27:32 +0100

    First try with example lines

--- Day1/script.js
+++ Day1/script.js
@@ -1,12 +1,17 @@
 /* 
-example
+example input:
 1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet 
 */
+// I should get values of these four lines as 12, 38, 15, and 77. Adding these together produces 142.
 
+let example = ["1abc2 ", "pqr3stu8vwx ", "a1b2c3d4e5f", "treb7uchet"];
+let regex = /\d/gm;
 
-const array1 = [];
-const array2 = [];
-const array3 = [];
+for (let element of example) {
+    // console.log(element);
+    console.log(element.trim().match(regex).join(""));   // output: 12, 38, 12345, 7 = need to find first number in each line from both left and right
+  }
+  
