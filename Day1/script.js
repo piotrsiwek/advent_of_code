ceab3dc43369fc25a9573d532de3b6945f62e8d7
piotrsiwek <59854137+piotrsiwek@users.noreply.github.com>
2024-03-06 00:34:55 +0100

    wrapped finding digits in to findFirstLast function

--- Day1/script.js
+++ Day1/script.js
@@ -6,10 +6,7 @@ a1b2c3d4e5f
 treb7uchet 
 */
 // I should get values of these four lines as 12, 38, 15, and 77. Adding these together produces 142.
-
-// let example = ["1abc2", "1pqr3stu8vwx ", "a1b2c3d4e5f", "treb7uchet"];
-//  let regex = /\d/gm;
-// // let regex = /^(\d)(.*?)(\d)$/;
+// let regex = /^(\d)(.*?)(\d)$/; = doesn't work
 
 // for (let element of example) {
 //     // console.log(element);
@@ -21,18 +18,31 @@ treb7uchet
 //   console.log(matchLast);
 
 // }
-  
+/* console.log(element.trim().match(regex).join("")) */
+/* console.log(element.trim().match(regex)) ; */
 
-let example = ["1abc2 ", "pqr3stu8vwx ", "a1b2c3d4e5f", "treb7uchet"];
-
+const example = ["1abc2 ", "pqr3stu8vwx ", "a1b2c3d4e5f", "treb7uchet"];
 const regex = /\d/gm; //regex to find all numbers in a string, global and multiline
 
-for (let element of example) {  //for each string in array
+// for (let element of example) {  //for each string in array
 
- /* console.log(element.trim().match(regex).join("")) */
-  /* console.log(element.trim().match(regex)) ; */
-  
-  let match = element.trim().match(regex) ; //create array from trimmed strings that contain only numbers
-  console.log(match[0]+match[match.length-1]); //from that array use first and last element which is first and last number in strings provided
-  } 
-  
+//   let match = element.trim().match(regex) ; //create array from trimmed strings that contain only numbers
+//   let pairs = (match[0]+match[match.length-1]); 
+//   let sum =0 
+//   match.forEach( (pairs) => { sum += pairs ;  });   
+//     console.log(pairs); 
+//     console.log(); //sum of all numbers in string
+// } 
+
+function findFirstLast(array) {
+    let result = [];
+
+    for (let element of array) { 
+        let match = element.trim().match(regex) ; //create array from trimmed strings that contain only numbers 
+        result.push(match[0] + match[match.length-1]); //from match array use first and last digit and add to result array
+    }
+    return result;
+}
+
+let pairs = findFirstLast(example);
+console.log(pairs);
