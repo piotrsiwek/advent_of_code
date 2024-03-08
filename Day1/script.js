281da1bbd6c36755095bbc6a71538ac000842109
piotrsiwek <59854137+piotrsiwek@users.noreply.github.com>
2024-03-08 23:59:48 +0100

    addded map of single digit nr

--- Day1/script.js
+++ Day1/script.js
@@ -71,3 +71,39 @@ function sumOfPairs(array) {
 
 let resultSumOfPairs = sumOfPairs(pairs);
 console.log(resultSumOfPairs);
+
+// Part 2
+
+// Example input:
+// two1nine
+// eightwothree
+// abcone2threexyz
+// xtwone3four
+// 4nineeightseven2
+// zoneight234
+// 7pqrstsixteen
+
+
+
+const spelledOut = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+//need function to find all spelled out numbers in a string
+
+//or function to convert spelled out numbers to digits
+
+const singleDigitMap = {
+    'zero': '0',
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+  };
+
+//   const words = data.toLowerCase().split(' ');
+
+//   console.log(words);
